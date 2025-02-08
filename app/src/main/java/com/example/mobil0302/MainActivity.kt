a86cbd79ea6df3044dc15f1e85eba5c144cc1256
qqSnitch <128982937+qqSnitch@users.noreply.github.com>
2025-02-08 11:58:01 +0300

    add mobile

---
+++ app/src/main/java/com/example/mobil0302/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.mobil0302
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import org.w3c.dom.Text
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var textOne = findViewById<TextView>(R.id.tvOne)
+        textOne.setOnClickListener(){
+            textOne.text = "textTwo"
+        }
+    }
+}
